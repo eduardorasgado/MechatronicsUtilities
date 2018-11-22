fce7eb5315db61364f8fd26fbd4c00c18a7cfd88
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-22 13:49:21 -0600

    double integration using trapezoidal method for 2D

--- Calculo/discreteIntegral/double_integral_trapezoidal.py
+++ Calculo/discreteIntegral/double_integral_trapezoidal.py
@@ -6,26 +6,33 @@ Algorithm can be find here:
 def main_function(x, y):
     return (3 * y)-(2 * (x*x))
 
-def sumation(interval, x, y):
+def sumation1(interval, y):
     i = 1
     result = 0.0
     while i != interval-1:
-        result += main_function(x, y)
+        result += main_function(i, y)
         i +=1
     return result 
 
+def sumation2(interval, x):
+    i = 1
+    result = 0.0
+    while i != interval-1:
+        result += main_function(x, i)
+    
 def double_integral_trapezoidal():
     #calculating the grid spacing
     h = (b - a) / float(m) # for y
     k = (d - c) / float(n) # for x
     
     first_component = (1 / 4.0)*h * k
-    second_component = main_function(a, c) +
-                        main_function(b, c) + 
-                        main_function(a, d) + 
-                        main_function(b, d)
+    second_component = main_function(a, c) + main_function(b, c)
+    second_component += main_function(a, d) + main_function(b, d)
 
-    thirdComponent = 0 #2 * (sumation())
+    thirdComponent = 2 * sumation1(m, c)
+    thirdComponent += 2 * sumation1(m, d)
+    thirdComponent += 2 * sumation2(n, a)
+    thirdComponent += 2 * sumation2(n, b)
 
 if __name__ == "__main__":
     a = 0
